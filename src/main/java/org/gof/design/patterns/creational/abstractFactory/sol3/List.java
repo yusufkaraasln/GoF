aa9a5e0bf1bff519b8806e6cda1190cb31c0cfd9
yusufkaraasln <yusufkaraasln@gmail.com>
2024-01-07 18:36:17 +0300

    abstract factory solution 3

---
+++ src/main/java/org/gof/design/patterns/creational/abstractFactory/sol3/List.java
@@ -0,0 +1,7 @@
+package org.gof.design.patterns.creational.abstractFactory.sol3;
+
+public abstract class List implements Component{
+
+	public abstract void paint();
+
+}
---
+++ src/main/java/org/gof/design/patterns/creational/abstractFactory/sol3/WinButton.java
@@ -0,0 +1,8 @@
+package org.gof.design.patterns.creational.abstractFactory.sol3;
+
+public class WinButton extends Button {
+    public void paint() {
+        System.out.println("Painting a Win Button!");
+    }
+}
+
