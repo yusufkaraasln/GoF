1fb78a7424fb50f41ee9b8fe50a37eeb78103bb6
yusufkaraasln <yusufkaraasln@gmail.com>
2024-02-11 20:38:31 +0300

    composite pattern

---
+++ src/main/java/org/gof/design/patterns/structural/composite/graphic/CompositeGraphic.java
@@ -0,0 +1,14 @@
+package org.gof.design.patterns.structural.composite.graphic;
+
+import java.util.*;
+
+public interface CompositeGraphic{	
+	
+	public void addGraphic(Graphic graphic);
+	
+	public void removeGraphic(Graphic graphic);
+	
+	public Collection<Graphic> getGraphics();
+	
+	public void listGraphic();
+}
---
+++ src/main/java/org/gof/design/patterns/structural/composite/graphic/Graphic.java
@@ -0,0 +1,11 @@
+package org.gof.design.patterns.structural.composite.graphic;
+
+public interface Graphic {
+
+	public void draw();
+
+	public void erase();
+
+	public void paint();
+
+}
